39b6b40821504d508425165070dfdc4569be8af8
KoungQ <koungq@KoungQui-MacBookAir.local>
2025-07-21 20:51:34 +0900

    init

---
+++ src/test/java/com/project/dorumdorum/DorumdorumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dorumdorum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorumdorumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
