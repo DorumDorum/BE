bc7b318bceed7ded590bb0bca3b50d8b166f5d6e
KoungQ <guu119@naver.com>
2025-07-21 20:51:34 +0900

    init

---
+++ src/main/java/com/project/dorumdorum/DorumdorumApplication.java
@@ -0,0 +1,13 @@
+package com.project.dorumdorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorumdorumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DorumdorumApplication.class, args);
+    }
+
+}
