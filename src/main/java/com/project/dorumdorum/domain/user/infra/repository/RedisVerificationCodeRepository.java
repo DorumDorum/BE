9ec35da608abddda789339a80a26619bad0529a8
KoungQ <guu119@naver.com>
2025-07-22 04:02:19 +0900

    feat: 이메일 인증 로직

--- src/main/java/com/project/dorumdorum/domain/user/infra/repository/RedisVerificationCodeRepository.java
+++ src/main/java/com/project/dorumdorum/domain/user/infra/repository/RedisVerificationCodeRepository.java
@@ -13,19 +13,20 @@ import java.util.Optional;
 public class RedisVerificationCodeRepository implements VerificationCodeRepository {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private static final String EMAIL_VERIFICATION = "EMAIL_VERIFICATION:";
 
     @Override
     public void save(String email, String code) {
-        redisTemplate.opsForValue().set(email, code, Duration.ofMinutes(10));
+        redisTemplate.opsForValue().set(EMAIL_VERIFICATION + email, code, Duration.ofMinutes(10));
     }
 
     @Override
     public Optional<String> findByEmail(String email) {
-        return Optional.empty();
+        return Optional.ofNullable(redisTemplate.opsForValue().get(EMAIL_VERIFICATION + email));
     }
 
     @Override
     public void delete(String email) {
-
+        redisTemplate.delete(EMAIL_VERIFICATION + email);
     }
 }
