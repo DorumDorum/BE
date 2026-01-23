3aebb862e5d989104365f6aed629bf4e55a34cdf
sbmax002 <jwooooy002@gmail.com>
2026-01-23 01:56:16 -0800

    fix: change the method of check existence message room

--- src/main/java/com/project/dorumdorum/domain/chat/domain/repository/MessageRoomRepository.java
+++ src/main/java/com/project/dorumdorum/domain/chat/domain/repository/MessageRoomRepository.java
@@ -1,7 +1,16 @@
 package com.project.dorumdorum.domain.chat.domain.repository;
 
 import com.project.dorumdorum.domain.chat.domain.entity.MessageRoom;
+import com.project.dorumdorum.domain.chat.domain.entity.MessageRoomStatus;
+import com.project.dorumdorum.domain.chat.domain.entity.MessageRoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface MessageRoomRepository extends JpaRepository<MessageRoom, Long> {
+    boolean existsByActiveDirectRoomKeyAndRoomTypeAndRoomStatusIn(
+        String activeDirectRoomKey,
+        MessageRoomType roomType,
+        Collection<MessageRoomStatus> statuses
+    );
 }
