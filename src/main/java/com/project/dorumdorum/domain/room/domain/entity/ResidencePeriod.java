8009b1e78403a1d6d19683396e7a1285f7e88da8
KoungQ <guu119@naver.com>
2026-02-02 22:11:02 +0900

    feat: 방 거주 기간 표기

---
+++ src/main/java/com/project/dorumdorum/domain/room/domain/entity/ResidencePeriod.java
@@ -0,0 +1,7 @@
+package com.project.dorumdorum.domain.room.domain.entity;
+
+public enum ResidencePeriod {
+    SEMESTER,      // 학기 (16주)
+    HALF_YEAR,     // 반기 (24주)
+    SEASONAL       // 계절학기
+}
