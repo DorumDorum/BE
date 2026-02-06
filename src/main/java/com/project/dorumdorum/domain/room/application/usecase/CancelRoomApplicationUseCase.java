943ecb3f3fbe9dca3a0dea1334988944195f0163
KoungQ <guu119@naver.com>
2026-02-06 19:45:29 +0900

    chore: 주석 제거

--- src/main/java/com/project/dorumdorum/domain/room/application/usecase/CancelRoomApplicationUseCase.java
+++ src/main/java/com/project/dorumdorum/domain/room/application/usecase/CancelRoomApplicationUseCase.java
@@ -15,9 +15,6 @@ public class CancelRoomApplicationUseCase {
     private final RoomService roomService;
     private final RoomRequestService roomRequestService;
 
-    /**
-     * 현재 로그인한 사용자가 특정 방에 보낸 가입 요청(지원 요청)을 취소한다.
-     */
     public void execute(String userNo, String roomNo) {
         Room room = roomService.findById(roomNo);
         roomRequestService.cancelJoinRequest(userNo, room);
