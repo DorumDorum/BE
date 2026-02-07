695ef6216d13ef43b7ae881d91d1fd67948df299
sbmax002 <jwooooy002@gmail.com>
2026-02-07 11:14:00 +0900

    feat: add notification event listener

--- src/main/java/com/project/dorumdorum/domain/chat/application/event/ChatEventPublisher.java
+++ src/main/java/com/project/dorumdorum/domain/chat/application/event/ChatEventPublisher.java
@@ -3,4 +3,6 @@ package com.project.dorumdorum.domain.chat.application.event;
 public interface ChatEventPublisher {
 
     void publishMessageSent(MessageSentEvent event);
+    void publishMessageRequestCreated(MessageRequestCreatedEvent event);
+    void publishMessageRequestDecided(MessageRequestDecidedEvent event);
 }
