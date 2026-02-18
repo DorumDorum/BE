ed8cd52b59317c74e715e50a06317111aef5aa4f
KoungQ <guu119@naver.com>
2026-02-19 01:20:52 +0900

    hotfix: CORS 경로 추가

--- src/main/java/com/project/dorumdorum/global/config/SecurityConfig.java
+++ src/main/java/com/project/dorumdorum/global/config/SecurityConfig.java
@@ -102,7 +102,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
             "http://localhost:3000",
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "https://dorumdorum.com",
+            "https://www.dorumdorum.com"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
