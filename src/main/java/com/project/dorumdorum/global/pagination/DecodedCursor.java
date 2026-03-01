110c3309810eeec993177a7f70ca6a1ce3b8ceb0
KoungQ <guu119@naver.com>
2026-03-01 15:11:23 +0900

    refactor: 커서 기반 페이지네이션 로직 수정

--- src/main/java/com/project/dorumdorum/global/pagination/DecodedCursor.java
+++ src/main/java/com/project/dorumdorum/global/pagination/DecodedCursor.java
@@ -3,7 +3,6 @@ package com.project.dorumdorum.global.pagination;
 import java.time.LocalDateTime;
 
 public record DecodedCursor(
-        Integer remaining,
         LocalDateTime createdAt,
         String pk
 ) {}
