b7d38d8e6785450e24de67541c87044caa2907d7
KoungQ <guu119@naver.com>
2026-03-03 00:15:12 +0900

    chore: 불필요한 어노테이션 제거

--- src/main/java/com/project/dorumdorum/domain/notification/domain/service/strategy/OnlineDeliveryStrategy.java
+++ src/main/java/com/project/dorumdorum/domain/notification/domain/service/strategy/OnlineDeliveryStrategy.java
@@ -1,12 +1,9 @@
 package com.project.dorumdorum.domain.notification.domain.service.strategy;
 
 import com.project.dorumdorum.domain.notification.domain.entity.NotificationDeliveryChannel;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class OnlineDeliveryStrategy implements NotificationDeliveryStrategy {
 
     @Override
