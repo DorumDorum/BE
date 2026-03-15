a74cdec370de3fe601476098e6d9ec948fc0293e
KoungQ <guu119@naver.com>
2026-03-15 16:40:54 +0900

    chore: 불필요한 파일 제거

--- src/main/java/com/project/dorumdorum/domain/notification/domain/entity/NotificationDeliveryOutboxStatus.java
+++
@@ -1,7 +0,0 @@
-package com.project.dorumdorum.domain.notification.domain.entity;
-
-public enum NotificationDeliveryOutboxStatus {
-    INIT,
-    SUCCESS,
-    FAIL
-}
--- src/main/java/com/project/dorumdorum/domain/notification/domain/repository/NotificationDeliveryOutboxRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.dorumdorum.domain.notification.domain.repository;
-
-import com.project.dorumdorum.domain.notification.domain.entity.NotificationDeliveryOutbox;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationDeliveryOutboxRepository extends JpaRepository<NotificationDeliveryOutbox, String>, NotificationDeliveryOutboxQueryRepository {
-}
