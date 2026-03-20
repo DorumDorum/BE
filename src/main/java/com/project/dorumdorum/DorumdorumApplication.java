acf97b15b10bf40b83d0411158d556ddd170dc9b
ydking0911 <ydking0911@gmail.com>
2026-03-20 22:08:33 +0900

    chore: MongoDB 의존성 제거 및 설정 정리

--- src/main/java/com/project/dorumdorum/DorumdorumApplication.java
+++ src/main/java/com/project/dorumdorum/DorumdorumApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @EnableJpaAuditing
-@EnableMongoAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableRetry
