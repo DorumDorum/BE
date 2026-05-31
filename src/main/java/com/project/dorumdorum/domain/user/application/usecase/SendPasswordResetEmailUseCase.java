24e0de49495b8f7502be33eccf96bd44895e19a8
KoungQ <guu119@naver.com>
2026-05-31 15:25:34 +0900

    fix: 사용자 열거 공격 방어

--- src/main/java/com/project/dorumdorum/domain/user/application/usecase/SendPasswordResetEmailUseCase.java
+++ src/main/java/com/project/dorumdorum/domain/user/application/usecase/SendPasswordResetEmailUseCase.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import static com.project.dorumdorum.global.exception.code.status.AuthErrorStatus.INVALID_EMAIL_DOMAIN;
-import static com.project.dorumdorum.global.exception.code.status.UserErrorStatus.EMAIL_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -20,17 +19,16 @@ public class SendPasswordResetEmailUseCase {
 
     /**
      * 비밀번호 재설정 인증 코드 발송
-     * - 가입된 이메일인지 확인
      * - 허용된 대학 이메일 도메인인지 검증
-     * - 인증 코드를 생성해 메일로 발송
+     * - 가입된 이메일인 경우에만 실제로 코드를 발송 (미가입 이메일도 동일한 200 응답 반환)
      */
     public void send(String email) {
-        if (!userService.isAlreadyRegistered(email)) {
-            throw new RestApiException(EMAIL_NOT_FOUND);
-        }
-
         if (!emailVerificationService.isAllowedUniversityEmail(email)) {
             throw new RestApiException(INVALID_EMAIL_DOMAIN);
+        }
+
+        if (!userService.isAlreadyRegistered(email)) {
+            return;
         }
 
         String code = secureRandomGenerator.generate();
