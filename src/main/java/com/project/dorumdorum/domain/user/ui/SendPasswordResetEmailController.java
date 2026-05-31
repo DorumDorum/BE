9a03a5163a175c163be40c223fd910aeb5e239cc
KoungQ <guu119@naver.com>
2026-05-31 15:39:59 +0900

    fix: 이메일 전송 로직 검증

--- src/main/java/com/project/dorumdorum/domain/user/ui/SendPasswordResetEmailController.java
+++ src/main/java/com/project/dorumdorum/domain/user/ui/SendPasswordResetEmailController.java
@@ -2,11 +2,14 @@ package com.project.dorumdorum.domain.user.ui;
 
 import com.project.dorumdorum.domain.user.application.usecase.SendPasswordResetEmailUseCase;
 import com.project.dorumdorum.domain.user.ui.spec.SendPasswordResetEmailApiSpec;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class SendPasswordResetEmailController implements SendPasswordResetEmailApiSpec {
@@ -15,7 +18,7 @@ public class SendPasswordResetEmailController implements SendPasswordResetEmailA
 
     @Override
     public ResponseEntity<Void> send(
-            @RequestParam String email
+            @NotBlank @RequestParam String email
     ) {
         sendPasswordResetEmailUseCase.send(email);
         return ResponseEntity.ok().build();
--- src/main/java/com/project/dorumdorum/domain/user/ui/VerifyPasswordResetCodeController.java
+++ src/main/java/com/project/dorumdorum/domain/user/ui/VerifyPasswordResetCodeController.java
@@ -2,11 +2,14 @@ package com.project.dorumdorum.domain.user.ui;
 
 import com.project.dorumdorum.domain.user.application.usecase.VerifyPasswordResetCodeUseCase;
 import com.project.dorumdorum.domain.user.ui.spec.VerifyPasswordResetCodeApiSpec;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class VerifyPasswordResetCodeController implements VerifyPasswordResetCodeApiSpec {
@@ -15,8 +18,8 @@ public class VerifyPasswordResetCodeController implements VerifyPasswordResetCod
 
     @Override
     public ResponseEntity<Void> verify(
-            @RequestParam String email,
-            @RequestParam String code
+            @NotBlank @RequestParam String email,
+            @NotBlank @RequestParam String code
     ) {
         verifyPasswordResetCodeUseCase.execute(email, code);
         return ResponseEntity.ok().build();
