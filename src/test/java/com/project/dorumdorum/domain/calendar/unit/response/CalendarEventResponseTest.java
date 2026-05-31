194ca802d91510f09bbb9756c4434fd9ed620647
KoungQ <guu119@naver.com>
2026-05-31 15:44:31 +0900

    test: 기존 테스트 코드 수정

--- src/test/java/com/project/dorumdorum/domain/calendar/unit/response/CalendarEventResponseTest.java
+++ src/test/java/com/project/dorumdorum/domain/calendar/unit/response/CalendarEventResponseTest.java
@@ -14,7 +14,7 @@ class CalendarEventResponseTest {
     @Test
     void record_AccessorsReturnValues() {
         LocalDate date = LocalDate.of(2026, 2, 14);
-        CalendarEventResponse response = new CalendarEventResponse(date, "event");
+        CalendarEventResponse response = new CalendarEventResponse(date, "event", null, null, null);
 
         assertThat(response.date()).isEqualTo(date);
         assertThat(response.title()).isEqualTo("event");
