d01059ca0b232f2b2099c70b62c3da75bb644847
KoungQ <guu119@naver.com>
2026-06-02 17:11:00 +0900

    fix: mapper 오류 수정

--- src/main/java/com/project/dorumdorum/domain/checklist/application/mapper/UserChecklistMapper.java
+++ src/main/java/com/project/dorumdorum/domain/checklist/application/mapper/UserChecklistMapper.java
@@ -15,6 +15,7 @@ public interface UserChecklistMapper {
     @Mapping(target = "userNo", source = "userNo")
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
     UserChecklist toUserChecklist(String userNo, CreateUserChecklistRequest request);
 
     @Mapping(target = "userChecklistNo", ignore = true)
